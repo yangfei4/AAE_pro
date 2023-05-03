63091f68f4dc932ca7f3a15c90ee44ac4b71d145
Jiahao Zhang <Davividzjh@gmail.com>
2023-05-03 18:17:37 -0500

    add scripts

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-# download checkpoints
-mkdir -p results;
-sh ./scripts/gdrive_download.sh 1-E8786Y2OmgbP09_T6iX9Vq0bbC4c-WA  checkpoints.tar.gz;
-tar -xvf checkpoints.tar.gz -C ./results/;
-rm ./checkpoints.tar.gz
 
 # download data
 mkdir -p data;
 sh ./scripts/gdrive_download.sh 1kBNP_PZ_MakYYBzj58WksFNg5U_c7yjS data.tar.gz;
-tar -xvf data.tar.gz -C ./data/;
+tar -xvf data.tar.gz -C ./;
 rm ./data.tar.gz
 
 # download detections
